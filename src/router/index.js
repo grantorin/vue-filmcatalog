ad7375d43b8a5e1ab651f865244f643e03f25d12
Gran Torino <red.chekist@gmail.com>
2019-08-25 13:02:57 +0300

    auth protect

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/Home.vue'
+import AuthProtect from '@/router/auth-protect'
+import Home from '@/views/Home'
 import Registration from '@/views/auth/Registration'
 import Authorization from '@/views/auth/Authorization'
 import NewFilm from '@/views/NewFilm'
@@ -23,22 +24,24 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
     },
     {
       path: '/reg',
       name: 'registration',
-      component: Registration
+      component: Registration,
+      beforeEnter: AuthProtect
     },
     {
       path: '/auth',
-      name: 'authorization',
-      component: Authorization
+      component: Authorization,
+      name: 'authorization'
     },
     {
       path: '/new',
       name: 'newfilm',
-      component: NewFilm
+      component: NewFilm,
+      beforeEnter: AuthProtect
     },
     {
       path: '/post/:id',
